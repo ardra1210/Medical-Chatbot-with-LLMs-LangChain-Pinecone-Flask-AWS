eaa5b6eb14e9a9fc4957dbcdb586d0763b363724
ardra1210 <ardra12kottodam@gmail.com.com>
2026-01-29 11:27:16 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ardra Sunil",
+    author_email="ardra12kottodam@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
